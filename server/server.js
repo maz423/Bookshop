d9c843b1776813cbc5163b93d0fd7a257a5d2e84
Brett Popowycz (bwp265) <bwp265@mail.usask.ca>
2022-02-23 11:37:05 -0600

    added basic frame work for cookies and sessions fixed issue with conencting to mongodb

--- server/server.js
+++ server/server.js
@@ -8,20 +8,18 @@ const path = require('path');
 const fs = require('fs');
 const http = require('http');
 const https = require('https');
+const bodyParser = require("body-parser");
+const MongoDBSession = require('connect-mongodb-session')(session);
+
+
 var options = {
     key : fs.readFileSync(path.join(__dirname, "perms", "client-key.pem")),
     cert : fs.readFileSync(path.join(__dirname, "perms", "client-cert.pem"))
 }
 
-const bodyParser = require("body-parser");
+
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-
 app.use(bodyParser.json());
-
-
-
 
 const cors = require('cors');
 app.use(cors());
@@ -34,20 +32,30 @@ const HOST = '0.0.0.0';
 // Helper
 const panic = (err) => console.error(err)
 
-//TODO: make this work with https
-// //Database stuff
-// const MongoClient = require('mongodb').MongoClient;
-// const DB_Url = "mongodb://admin:admin@mongodb1:3306/";
-// var con;
-// // Connect to database
-// MongoClient.connect(DB_Url, (err, db) =>{
-//     //This will connec thte node server to the mongo server on startup
-//     if (err) throw err;
-//     //We can change the name later
-//     con = db.db("ecomDB");
-// });
+//Database stuff
+const MongoClient = require('mongodb').MongoClient;
+const DB_Url = 'mongodb://admin:admin@mongodb:27017/';
+var con;
 
+// Connect to database
+MongoClient.connect(DB_Url, (err, db) =>{
+    //This will connec thte node server to the mongo server on startup
+    if (err) throw err;
+    //We can change the name later
+    con = db.db("ecomDB");
+    console.log("MongoDB Connected");
+});
+const store = new MongoDBSession({
+    uri : DB_Url,
+    collection : 'mySessions'
+});
 
+app.use(session({
+    secret : 'key that will sign cookie',
+    resave : false,
+    saveUninitialized : false,
+    store : store
+}));
 
 //Login and Registration stuff
 app.post("/login", (req, res)=>{
@@ -100,8 +108,10 @@ app.post("/register", (req, res)=> {
     });
 });
  
-//This is just to test if https server is working
+//This is just to testing stuff
 app.get('/', (req, res) => {
+    req.session.isAuth = true;
+    console.log(req.session);
     res.send("Success");
 });
 
@@ -109,9 +119,9 @@ app.use('/', express.static('pages'));
 
 //FIXME: HTTPS server giving Error code: SSL_ERROR_RX_RECORD_TOO_LONG on firefox
 //Create the Http server
-http.createServer(app).listen(PORT);
+http.createServer(app).listen(PORT, HOST);
 //Create the identical https server
-https.createServer(options, app).listen(443);
+https.createServer(options, app).listen(443, HOST);
 
 //app.listen(PORT, HOST);
 
