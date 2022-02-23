11c6c97be3448bc1ee502ffae37eaa57d358c9bf
Brett Popowycz (bwp265) <bwp265@mail.usask.ca>
2022-02-22 22:04:26 -0600

    made changes to try and get react on https

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import {Login} from './components/Login.js'
 //import {Register} from './components/Register.js';
 
-
 function App() {  
   
   
