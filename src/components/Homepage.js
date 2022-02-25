5c4b26282aa50ba25ef393ac742521b4f7a81fae
Mohammad Zargar <maz423@usask.ca>
2022-02-24 18:22:13 -0600

    added Homepage.

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,6 +5,8 @@ import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import { Link } from 'react-router-dom';
+import {Navi} from './Navi'
+
 
 
 
@@ -16,15 +18,20 @@ const handleSubmitClick = (e) => { //handle submit event.
         
 
 return (
+
+ 
     
     <div className='bg'>
+        
+       
+        <h1 className='logo'>Bookshop</h1>
     <Form className="d-flex">
         <Form.Control
           type="search"
-          placeholder="Enter your location"
+          placeholder="Enter your location to get started"
           className="me-2"
           aria-label="Search"
-          size='sm'
+          size='md'
         />
        <Button variant="success">Submit</Button>{' '}
     
