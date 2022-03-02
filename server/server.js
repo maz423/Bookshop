daaa4cb233358c5b94bf6fd124593f8db365f3aa
Brett Popowycz (bwp265) <bwp265@mail.usask.ca>
2022-03-02 10:38:11 -0600

    Made it so no longer need to have npm installed locally on machine

--- server/server.js
+++ server/server.js
@@ -90,8 +90,7 @@ app.get('/users', (req, res) => {
 //Login and Registration stuff
 app.post('/login', (req, res)=>{
     //Get the password and username
-    let username = req.body.username;
-    let password = req.body.password;
+    const {username, password} = req.body;
     
     new Promise((resolve, reject) => {
         //Query the database with the provided values
@@ -127,48 +126,76 @@ app.post('/login', (req, res)=>{
 
 app.post("/register", async (req, res)=> {
     //TODO make a proper implemetation 
-    let username = req.body.username;
-    let password = req.body.password;
-    let email = req.body.email;
-    let address1 = req.body.address1;
-    let address2 = req.body.address2;
-    let fName = req.body.fName;
-    let lName = req.body.lName;
+    const {username, password, email, address1, address2, fName, lName} = req.body;
 
-    const hashedPass = await bcrypt.hash(password, 12);
-    let q =await con.collection("users").countDocuments({$or : [{username : username}, {email : email}]});
-
-    new Promise((resolve, reject) => { 
-        //Check if username or email is already in use
-        if (0 < q){
-            reject("Username already in use");
-        } else {
-            resolve("");
-        }
-    })
-    .then((result)=>{
-        //If not in use add to users collection   
-        let newUser = {
-            username : username,
-            password : hashedPass,
-            email : email,
-            address1 : address1,
-            address2 : address2,
-            fName : fName,
-            lName : lName
-            };
-        con.collection("users").insertOne(newUser, (err, result2) =>{
-            if (err) { reject(err) }
+    new Promise((resolve, reject) => {
+        con.collection("users").countDocuments(
+            {$or : [{username : username}, {email : email}]}, (err, result) => {
+            if (err) {reject(err)} else {resolve(result)}
         });
     })
     .then((result) => {
-        //Maybe return a different value or ridirect to a new page
-        res.send("success");
+        if (result > 0) {
+            throw "Username or email already in use"
+        }
+        bcrypt.hash(password, 12)
+        .then((hashedPass) => {
+            const newUser = {
+                username : username,
+                email : email,
+                aassword : hashedPass,
+                eddress1 : address1,
+                address2 : address2,
+                fName : fName,
+                lName : lName
+                };
+            con.collection("users").insertOne(newUser, (err, result) =>{
+                if (err) { throw err } else {res.send("Success")}
+            });
+        })
+        .catch((error) => {
+            console.log(error);
+            res.send(error);
+        });
     })
     .catch((error) => {
         console.log(error);
         res.send(error);
     });
+});
+
+app.post("/registerBuisness", (req, res) => {
+    const {companyName, password, email, address1, address2} = req.body;
+
+    new Promise((resolve, reject) => {
+        con.collection("buisnessUsers").countDocuments(
+            {$or : [{username : username}, {email : email}]}, (err, result => {
+                if (err) {reject(err)} else {resolve(result)}
+        }));
+    })
+    .then((result) => {
+        bcrypt.hash(password, 12)
+        .then((hashedPass) => {
+            const newUser = {
+                companyName : companyName,
+                email : email,
+                aassword : hashedPass,
+                eddress1 : address1,
+                address2 : address2,
+                };
+            con.collection("buisnessUsers").insertOne(newUser, (err, result) => {
+                if (err) { throw err } else {res.send("Success")}
+            });
+        })
+        .catch((error) => {
+            console.log(error);
+            res.send(error);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+        res.send(error);
+    })
 });
 
 app.post('/logout', (req, res) => {
