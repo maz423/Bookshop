62c134db8a12c09ead58fa513eaf04d6be8d0b16
Mohammad Zargar <maz423@usask.ca>
2022-03-26 17:42:25 -0600

    Minor route fixes.

--- src/App.js
+++ src/App.js
@@ -266,7 +266,7 @@ useEffect(() => {
           <Route exact path='/listing/:listingID' element={<ListingView/>} component={listingID}/>
           <Route exact path='/user' element={<AccountView/>} />
           <Route exact path='/wishlist' element={<Wishlist/>}/>
-          <Route exact path='Mylistings' element={<MyListings />}/>
+          <Route exact path='Mylistings' element={<MyListings  bookstore = {0} user = {1}/>}/>
           <Route exact path='updatelisting' element={<Createlisting update = {1} />}/>
         
     
@@ -291,7 +291,7 @@ useEffect(() => {
        <Route exact path='/' element={<Homepage/>} />
        <Route exact path='/logout' element={<Logout  set = {setLoggedIn} admin = {setIsAdmin} bookstore = {setbookStore}/>}/>
        <Route exact path='/createlisting' element={<Createlisting update = {0}/>}/>
-       <Route exact path='Mylistings' element={<MyListings />}/>
+       <Route exact path='Mylistings' element={<MyListings bookstore = {1} user = {0} />}/>
     </Routes>)
     : <></>
     
--- src/components/MyListings.js
+++ src/components/MyListings.js
@@ -4,10 +4,18 @@ import { ListOfListings } from './ListOfListings';
 import { useState } from 'react';
 
 
-//grab listings of user.
 
-function MyListings (){
+
+function MyListings (props){
     const [Listings,setListings] = useState('')
+
+    //grab listings of user.
+    if (props.bookstore == 1 && props.user == 0){ //grab Listings posted by Bookstore 
+
+    }
+    else{ //grab listings from user
+
+    }
 
     return (
         <div><h1> Users/Bookstores Listings will be shown here</h1>
